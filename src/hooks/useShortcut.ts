54f0312e97e4c02de54523faeefbc89ff5e0d02d
Harsh Singh <hi.harsh@protonmail.ch>
2022-07-24 01:47:35 -0400

    Added the useShortcut hook

---
+++ src/hooks/useShortcut.ts
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export const useShortcut = (
+  targetKey: string,
+  modifier?: 'shift' | 'ctrl'
+): boolean => {
+  const [keyPressed, setKeyPressed] = useState(false)
+
+  const downHandler = useCallback((event: KeyboardEvent) => {
+    if (event.key === targetKey) {
+      if (modifier === 'shift' && event.shiftKey) {
+        event.preventDefault()
+        setKeyPressed(true)
+      } else if (modifier === 'ctrl' && event.ctrlKey) {
+        event.preventDefault()
+        setKeyPressed(true)
+      } else {
+        event.preventDefault()
+        setKeyPressed(true)
+      }
+    }
+  }, [])
+
+  const upHandler = useCallback((event: KeyboardEvent) => {
+    if (event.key === targetKey) setKeyPressed(false)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler)
+    window.addEventListener('keyup', upHandler)
+
+    return () => {
+      window.removeEventListener('keydown', downHandler)
+      window.removeEventListener('keyup', upHandler)
+    }
+  }, [])
+
+  return keyPressed
+}
