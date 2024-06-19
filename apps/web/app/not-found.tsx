2d14ccc3f9edbeb69913074e69a2dc4cb1b33c10
Harsh Singh <hi.harsh@protonmail.ch>
2024-06-19 01:36:56 -0500

    fix 404 text color

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,5 +1,5 @@
 export default () => (
   <main className="flex h-screen items-center justify-center">
-    <h1 className="text-9xl font-bold text-slate-900">404</h1>
+    <h1 className="text-9xl font-bold text-slate-950 dark:text-slate-50">404</h1>
   </main>
 );
